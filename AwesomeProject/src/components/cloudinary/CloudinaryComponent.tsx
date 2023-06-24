4fcfffaeac9494504054922483deb6e5c72db049
Matias Hernandez <matias2k00@gmail.com>
2023-06-23 22:39:03 -0300

    upload image cloudinary

--- AwesomeProject/src/components/cloudinary/CloudinaryComponent.tsx
+++ AwesomeProject/src/components/cloudinary/CloudinaryComponent.tsx
@@ -1,77 +1,94 @@
 import React, {useState} from 'react';
-import {View, Text, Button, Image} from 'react-native';
-import {SafeAreaView} from 'react-native';
-import ImagePicker, {launchImageLibrary} from 'react-native-image-picker';
-import axios from 'axios';
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
-// const cld = new Cloudinary({
-//   cloud: {
-//     cloudName: 'dtw0xzty5',
-//   },
-// });
+const CloudinaryComponent: React.FC = () => {
+  const [photo, setPhoto] = useState(
+    'https://res.cloudinary.com/dhgn9tq4j/image/upload/v1666764536/cld-sample-2.jpg',
+  );
 
-const apiKey = 141258927571659;
-const apiSecret = 'pgB_7FU76nMCQ3ttZ2ejhPAXhfg';
-const cloudName = 'dtw0xzty5';
-const uploadPreset = 'hjuft8mh';
-
-const CloudinaryComponent = () => {
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
-
-  const pickImage = async () => {
-    launchImageLibrary(
-      {mediaType: 'photo', includeBase64: true, maxHeight: 400, maxWidth: 400},
-      async response => {
-        if (response.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (response.errorCode) {
-          console.log('ImagePicker Error: ', response.errorCode);
-        } else {
-          console.log(response);
-          const base64Image = await response.assets![0].uri;
-          const base64Img = `data:image/jpg;base64,${base64Image}`;
-
-          const uploadedImageData = new FormData();
-          uploadedImageData.append('upload_preset', uploadPreset);
-          uploadedImageData.append('file', base64Image);
-          // uploadedImageData.console.log('', uploadedImageData.getParts());
-
-          try {
-            const uploadedImage = await axios.post(
-              `https://api.cloudinary.com/v1_1/dtw0xzty5/image/upload/`,
-              uploadedImageData,
-              {
-                headers: {
-                  'Content-Type': 'multipart/form-data',
-                  'X-Requested-With': 'XMLHttpRequest',
-                  'X-API-Key': apiKey,
-                },
-              },
-            );
-
-            setSelectedImage(uploadedImage.data.secure_url);
-          } catch (error) {
-            console.log('Error uploading image: ', error);
-          }
-        }
-      },
-    );
+  const cloudinaryUpload = (photo: any) => {
+    const data = new FormData();
+    data.append('file', photo);
+    data.append('upload_preset', 'hjuft8mh');
+    data.append('cloud_name', 'dtw0xzty5');
+    fetch('https://api.cloudinary.com/v1_1/dtw0xzty5/upload', {
+      method: 'post',
+      body: data,
+    })
+      .then(res => {
+        res.json();
+        setPhoto(photo.uri);
+      })
+      .catch(err => {
+        console.log('err.response.data', err);
+        Alert.alert('An Error Occured While Uploading', err);
+      });
   };
 
   return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-      <Button title="Pick Image" onPress={pickImage} />
-      <View>
-        {selectedImage && (
-          <Image
-            source={{uri: selectedImage}}
-            style={{width: 300, height: 300}}
-          />
-        )}
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', width: 300}}>
+      <Image source={{uri: photo}} style={styles.fotito} />
+      <View style={{alignItems: 'center', width: 300}}>
+        <Text style={styles.uploadHeaderStyle}>Upload Picture</Text>
+        <TouchableOpacity
+          onPress={() => {
+            launchImageLibrary(
+              {mediaType: 'photo', selectionLimit: 1},
+              response => {
+                console.log('response', response);
+                if (response.didCancel) {
+                  console.log('User cancelled image picker');
+                } else if (response.errorMessage) {
+                  console.log('ImagePicker Error: ', response.errorMessage);
+                } else {
+                  const uri = response?.assets?.[0].uri;
+                  const type = response?.assets?.[0].type;
+                  const name = response?.assets?.[0].fileName;
+                  const source = {
+                    uri,
+                    type,
+                    name,
+                  };
+                  cloudinaryUpload(source);
+                }
+              },
+            );
+          }}
+          style={styles.openGalaryStyle}>
+          <Text>Open Galary</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  uploadHeaderStyle: {
+    fontStyle: 'normal',
+    color: 'white',
+    fontWeight: 'bold',
+    lineHeight: 24,
+    fontSize: 16,
+  },
+  openGalaryStyle: {
+    backgroundColor: 'orange',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  fotito: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+});
 
 export default CloudinaryComponent;

6dd325084291e14a9784a5204870e29630e879e0
Matias Hernandez <matias2k00@gmail.com>
2023-06-26 19:45:04 -0300

    cloudinary component upload image

--- AwesomeProject/src/components/cloudinary/CloudinaryComponent.tsx
+++ AwesomeProject/src/components/cloudinary/CloudinaryComponent.tsx
@@ -14,7 +14,6 @@ const CloudinaryComponent: React.FC = () => {
   const [photo, setPhoto] = useState(
     'https://res.cloudinary.com/dhgn9tq4j/image/upload/v1666764536/cld-sample-2.jpg',
   );
-
   const cloudinaryUpload = (photo: any) => {
     const data = new FormData();
     data.append('file', photo);
@@ -24,9 +23,10 @@ const CloudinaryComponent: React.FC = () => {
       method: 'post',
       body: data,
     })
-      .then(res => {
-        res.json();
-        setPhoto(photo.uri);
+      .then(res => res.json())
+      .then(resData => {
+        const uploadedImageUrl = resData.secure_url;
+        setPhoto(uploadedImageUrl);
       })
       .catch(err => {
         console.log('err.response.data', err);
@@ -34,38 +34,38 @@ const CloudinaryComponent: React.FC = () => {
       });
   };
 
+  console.log(photo);
+
+  const handleUploadImage = () => {
+    launchImageLibrary({mediaType: 'photo', selectionLimit: 1}, response => {
+      console.log('response', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.errorMessage) {
+        console.log('ImagePicker Error: ', response.errorMessage);
+      } else {
+        const uri = response?.assets?.[0].uri;
+        const type = response?.assets?.[0].type;
+        const name = response?.assets?.[0].fileName;
+        const source = {
+          uri,
+          type,
+          name,
+        };
+        cloudinaryUpload(source);
+      }
+    });
+  };
+
   return (
     <SafeAreaView
       style={{flex: 1, justifyContent: 'center', width: 400, height: 500}}>
-      <Image source={{uri: photo}} style={StyleSheet.absoluteFill} />
       <View style={{alignItems: 'center'}}>
         <Text style={styles.uploadHeaderStyle}>Upload Picture</Text>
         <TouchableOpacity
-          onPress={() => {
-            launchImageLibrary(
-              {mediaType: 'photo', selectionLimit: 1},
-              response => {
-                console.log('response', response);
-                if (response.didCancel) {
-                  console.log('User cancelled image picker');
-                } else if (response.errorMessage) {
-                  console.log('ImagePicker Error: ', response.errorMessage);
-                } else {
-                  const uri = response?.assets?.[0].uri;
-                  const type = response?.assets?.[0].type;
-                  const name = response?.assets?.[0].fileName;
-                  const source = {
-                    uri,
-                    type,
-                    name,
-                  };
-                  cloudinaryUpload(source);
-                }
-              },
-            );
-          }}
+          onPress={handleUploadImage}
           style={styles.openGalaryStyle}>
-          <Text>Open Galary</Text>
+          <Image source={{uri: photo}} style={StyleSheet.absoluteFill} />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -85,6 +85,8 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     borderRadius: 10,
+    height: 200,
+    width: 100,
   },
 });
 
